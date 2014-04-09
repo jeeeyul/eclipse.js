72943b85fa228ad58cc910ce5b62c13b4da828a4
Jeeeyul <Jeeeyul@gmail.com>
2014-04-09 15:58:08 +0900

    feature project was added

--- net.jeeeyul.eclipsejs/libraries/both/embeded-browser.js
+++ net.jeeeyul.eclipsejs/libraries/both/embeded-browser.js
@@ -3,25 +3,26 @@
  *            parent
  */
 ejs.EmbededBrowser = function(parent) {
+	var me = this;
 	this.handle = new org.eclipse.swt.browser.Browser(parent, SWT.NORMAL);
+
+	new JavaAdapter(org.eclipse.swt.browser.BrowserFunction, {
+		"function" : function(args) {
+			var data = JSON.parse(args[0]);
+
+			if (typeof me.messageHandler === "function") {
+				me.messageHandler.call(me, data);
+			}
+		}
+	}, this.handle, "__sendMessage");
 };
 
 /**
- * Insert a bridge function into browser.
- * 
- * @param name
- *            function name that will be referred in browser script.
- * @param {Function}
- *            handler Function script. Types of arguments and return value must
- *            be primitive or {@link Array}. Otherwise it will be fail.
+ * @param Function
+ *            handler
  */
-ejs.EmbededBrowser.prototype.addFunction = function(name, handler) {
-	var me = this;
-	new JavaAdapter(org.eclipse.swt.browser.BrowserFunction, {
-		"function" : function() {
-			return handler.apply(me, Array.prototype.splice.call(arguments, 0));
-		}
-	}, this.handle, name);
+ejs.EmbededBrowser.prototype.onMessage = function(handler) {
+	this.messageHandler = handler;
 };
 
 /**
@@ -33,16 +34,7 @@ ejs.EmbededBrowser.prototype.setURL = function(url) {
 	this.handle.setUrl(url);
 };
 
-/**
- * Runs a script in embeded browser.
- * 
- * @param {Function}
- *            run function to run in browser. All contents of function must not
- *            refer outside of function. this function will be serialized and
- *            passed to browser. It can have a return value.
- * @returns
- */
-ejs.EmbededBrowser.prototype.eval = function(run) {
-	var script = "(" + String(run) + ")();"
-	return this.handle.evaluate(script);
+ejs.EmbededBrowser.prototype.send = function(data) {
+	var serializedData = JSON.stringify(data);
+	this.handle.evaluate('if(typeof onMessage === "function") onMessage(' + serializedData + ')');
 };
